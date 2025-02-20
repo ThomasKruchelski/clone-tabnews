6103ebfb44e003b58507c3d629c6f4489b4cc860
Thomas Kruchelski <106840812+ThomasKruchelski@users.noreply.github.com>
2025-02-20 17:17:42 +0000

    frase consertada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Hello World</h1>
+    return <div>
+        <h1>Oi meu amor to estudando junto com vc para algum dia comprarmos uma mansão ainda maior pro Jijieopink. Amo vc, Força!!!</h1>
+        </div>
 }
 
 export default Home;
