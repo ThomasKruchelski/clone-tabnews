ddf1d7f3bdf7ac6b00fa107c9550c69d531274ae
Thomas Kruchelski <106840812+ThomasKruchelski@users.noreply.github.com>
2026-05-12 21:24:44 +0000

    add `serverVersion` and connections information on `/api/v1/status` page

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,27 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  // const result = await database.query("SELECT 1 + 1 as sum");
-  // console.log(result.rows);
+  const getServerVersion = await database.query("SHOW server_version;");
+  const serverVersion = getServerVersion.rows[0].server_version;
+  // console.log(serverVersion);
+
+  const getMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = getMaxConnections.rows[0].max_connections;
+  // console.log(getMaxConnections.rows[0].max_connections);
+
+  const getUsedConnections = await database.query(
+    "SELECT pid, usename, client_addr, state, query FROM pg_stat_activity WHERE state != 'idle';"
+  );
+  const usedConnections = getUsedConnections.rows.length;
+  // console.log(usedConnections);
+
   const updatedAt = new Date().toISOString();
 
   response.status(200).json({
     updated_at: updatedAt,
+    server_version: serverVersion,
+    max_connections: maxConnections,
+    used_connections: usedConnections,
   });
 }
 
